9276e352ffb9a7663c024802c36f4dee7cabd1d9
Syncdeenoc <151070040+Syncdeenoc@users.noreply.github.com>
2023-11-16 22:00:31 +0300

    Add files via upload

---
+++ Studentdetails.java
@@ -0,0 +1,21 @@
+  public class  Studentdetails{
+
+    
+    public static void main(String[] args) {
+    String course = "Programming and Development"; 
+    String name = "Derrick Hope";
+    String reg  = "22/07335";
+    String dob = "2000-05-1"; 
+    String vision = "Crafting the Future";
+    String favLang = "Java";
+
+    System.out.println("Course: " + course);
+    System.out.println("Name: " + name);
+    System.out.println("Registration Number: " + reg);
+    System.out.println("Date of Birth: " + dob);
+    System.out.println("Vision: " + vision);
+    System.out.println("Favorite Language: " + favLang);
+
+    }
+    
+}
